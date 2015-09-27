29464d0102132c69154c8b2b062e4b38a191d2b7
Jens John <dev@2ion.de>
2015-09-27 18:13:51 +0200

    af_center: Remove

--- wscript_build.py
+++ wscript_build.py
@@ -101,7 +101,6 @@ def build(ctx):
         ( "audio/decode/dec_audio.c" ),
         ( "audio/filter/af.c" ),
         ( "audio/filter/af_bs2b.c",              "libbs2b" ),
-        ( "audio/filter/af_center.c" ),
         ( "audio/filter/af_channels.c" ),
         ( "audio/filter/af_delay.c" ),
         ( "audio/filter/af_drc.c" ),
